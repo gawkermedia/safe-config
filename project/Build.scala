163ebda7c4e6a82dc385cdf690d2d9e36c3788e0
ChrisNeveu <cneveu@gawker.com>
2016-01-21 10:52:53 -0500

    Set version to 1.1.1-SNAPSHOT.

--- project/Build.scala
+++ project/Build.scala
@@ -36,7 +36,7 @@ object Build extends Build {
     file("."),
     settings = Defaults.defaultSettings ++ scalariformSettings ++ wartremoverSettings ++ Seq(
       organization := "com.kinja",
-      version      := "1.1.0",
+      version      := "1.1.1-SNAPSHOT",
       scalaVersion := "2.11.6",
       pomExtra := pomStuff,
       scalacOptions ++= Seq(
