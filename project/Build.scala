bc0049260718d31e7e04e15b6b7647be78e5d1b1
Sean Sullivan <ssullivan@gilt.com>
2018-07-03 07:10:30 -0700

    Scala 2.11.12

--- project/Build.scala
+++ project/Build.scala
@@ -38,7 +38,7 @@ object Build extends Build {
       organization := "com.kinja",
       version      := "1.1.2-SNAPSHOT",
       scalaVersion := "2.12.6",
-      crossScalaVersions := Seq("2.12.6", "2.11.8"),
+      crossScalaVersions := Seq("2.12.6", "2.11.12"),
       pomExtra := pomStuff,
       scalacOptions ++= Seq(
         "-deprecation",          // Show details of deprecation warnings.
